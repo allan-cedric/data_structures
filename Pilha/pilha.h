3f12ae9b66062b6e52ad737438ea3c7930fa0e9f
allan-cedric <allan.py3000@gmail.com>
2020-04-23 13:24:18 -0300

    Update de Reorganização/Documentação

--- Pilha/pilha.h
+++ Pilha/pilha.h
@@ -1,18 +1,57 @@
-#define MAX 5 /*Tamanho máximo da pilha, pode alterar se quiser */
+/* 
+   Header file : 'pilha.h'
+   Escrito por : Allan Cedric G.B. Alves da Silva
+   Profile : Aluno de graduação do curso de Ciência da Computação (UFPR)
+   GRR : 20190351
+*/
 
-typedef struct tad_pilha{
+#ifndef __PILHA_H_
+#define __PILHA_H_
+
+#include <stdio.h>
+
+#define MAX 100 /* Tamanho da pilha */
+
+typedef struct pilha_t
+{
 	int v[MAX];
-	int indice_topo;
-}tad_pilha;
+	int topo;
+} pilha_t;
 
-void inicializa_pilha(tad_pilha *p);
+/*
+	Função que inicializa uma pilha.
+	Se ela já existir, então será sobrescrita uma nova.
+*/
+void inicializa_pilha(pilha_t *p);
 
-int pilha_vazia(tad_pilha *p);
+/*
+	Função que verifica se a pilha está vazia ou não.
+	Se estiver vazia retorna 1, senão 0.
+*/
+int pilha_vazia(pilha_t *p);
 
-int tamanho_pilha (tad_pilha *p);
+/*
+	Função que retorna o tamanho da pilha.
+*/
+int tamanho_pilha(pilha_t *p);
 
-int empilha(int x,tad_pilha *p);
+/*
+	Retorna 1 se o empilhamento do elemento 'x' foi bem sucedido,
+	e 0 em caso contrário.
+*/
+int empilha(int x, pilha_t *p);
 
-int desempilha(int *t,tad_pilha *p);
+/*
+	Retorna 1 se o desempilhamento foi bem sucedido,
+	e 0 em caso contrário.
+*/
+int desempilha(pilha_t *p);
 
-int topo (int *t,tad_pilha *p);
+/*
+	Retorna o elemento do topo da pilha na variável 't'.
+	A função retorna 1 se a operação foi bem sucedida, e
+	0 em caso contrário.
+*/
+int topo(int *t, pilha_t *p);
+
+#endif
