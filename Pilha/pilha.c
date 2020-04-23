316838ec2e3642c5b3dd9e3986b727d71b9afa49
allan-cedric <allan.py3000@gmail.com>
2020-04-23 13:28:16 -0300

    Update de Reorganização/Documentação

--- Pilha/pilha.c
+++ Pilha/pilha.c
@@ -1,48 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* 
+   Source file : 'pilha.c'
+   Escrito por : Allan Cedric G.B. Alves da Silva
+   Profile : Aluno de graduação do curso de Ciência da Computação (UFPR)
+   GRR : 20190351
+*/
+
 #include "pilha.h"
 
-void inicializa_pilha(tad_pilha *p){
-	p->indice_topo = -1;
+void inicializa_pilha(pilha_t *p)
+{
+	p->topo = -1;
 }
 
-int pilha_vazia(tad_pilha *p){
-	return(p->indice_topo == -1);
+int pilha_vazia(pilha_t *p)
+{
+	return (p->topo == -1);
 }
 
-int tamanho_pilha(tad_pilha *p){
-	return(p->indice_topo + 1);
+int tamanho_pilha(pilha_t *p)
+{
+	return (p->topo + 1);
 }
 
-/* Se o tamanho da pilha não foi extrapolado,
-   então empilha o elemento x */
-int empilha(int x,tad_pilha *p){
-
-	if(tamanho_pilha(p) < MAX){
-		p->v[++(p->indice_topo)] = x;
+int empilha(int x, pilha_t *p)
+{
+	if (tamanho_pilha(p) < MAX)
+	{
+		p->v[++(p->topo)] = x;
 		return 1;
 	}
-
 	return 0;
 }
 
-/*Retorna o topo da pilha em t, e o desempilha */
-int desempilha(int *t,tad_pilha *p){
-	
-	if(pilha_vazia(p))
+int desempilha(pilha_t *p)
+{
+	if (pilha_vazia(p))
 		return 0;
-
-	*t = p->v[(p->indice_topo)--];
+	p->topo--;
 	return 1;
 }
 
-/*Retorna o topo da pilha em t */
-int topo(int *t,tad_pilha *p){
-	
-	if(pilha_vazia(p))
+int topo(int *t, pilha_t *p)
+{
+	if (pilha_vazia(p))
 		return 0;
-
-	*t = p->v[p->indice_topo];
+	*t = p->v[p->topo];
 	return 1;
-
 }
