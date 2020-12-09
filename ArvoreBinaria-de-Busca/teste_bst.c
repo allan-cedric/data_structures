b03bf353cf7dfbf5622460018b00f9757cfea01e
Allan Cedric <allan.py3000@gmail.com>
2020-12-09 10:35:31 -0300

    Nomenclatura de 'chave' é mais adequada

--- ArvoreBinaria-de-Busca/teste_bst.c
+++ ArvoreBinaria-de-Busca/teste_bst.c
@@ -18,20 +18,20 @@ int main()
 	{
 		printf("\nBST está vazia!\n");
 		rootBst = insertNodeBST(rootBst, 5);
-		printf("Criando nodo raiz com valor 5...\n");
+		printf("Criando nodo raiz com chave 5...\n");
 		printf("Nodo raiz criado com sucesso!\n\n");
 
-		printf("Inserindo nodo com valor 1...\n");
+		printf("Inserindo nodo com chave 1...\n");
 		rootBst = insertNodeBST(rootBst, 1);
-		printf("Inserindo nodo com valor 9...\n");
+		printf("Inserindo nodo com chave 9...\n");
 		rootBst = insertNodeBST(rootBst, 9);
-		printf("Inserindo nodo com valor 10...\n");
+		printf("Inserindo nodo com chave 10...\n");
 		rootBst = insertNodeBST(rootBst, 10);
-		printf("Inserindo nodo com valor 0...\n");
+		printf("Inserindo nodo com chave 0...\n");
 		rootBst = insertNodeBST(rootBst, 0);
-		printf("Inserindo nodo com valor 4...\n");
+		printf("Inserindo nodo com chave 4...\n");
 		rootBst = insertNodeBST(rootBst, 4);
-		printf("Inserindo nodo com valor 8...\n");
+		printf("Inserindo nodo com chave 8...\n");
 		rootBst = insertNodeBST(rootBst, 8);
 	}
 
@@ -53,26 +53,26 @@ int main()
 	/* Elementos extremos: máximo e mínimo */
 	BST *genericNodeBst;
 	genericNodeBst = maxKeyBST(rootBst);
-	printf("\n\nMaior valor: %i", genericNodeBst->key);
+	printf("\n\nMaior chave: %i", genericNodeBst->key);
 	genericNodeBst = minKeyBST(rootBst);
-	printf("\nMenor valor: %i\n", genericNodeBst->key);
+	printf("\nMenor chave: %i\n", genericNodeBst->key);
 
 	/* Teste de busca de elementos na BST */
-	printf("\nBuscando o valor 4...\n");
+	printf("\nBuscando a chave 4...\n");
 	genericNodeBst = searchBST(rootBst, 4);
 	if (genericNodeBst)
-		printf("Valor ENCONTRADO: %i\n", genericNodeBst->key);
+		printf("Chave ENCONTRADA: %i\n", genericNodeBst->key);
 	else
-		printf("Valor NÃO encontrado\n");
+		printf("Chave NÃO encontrada\n");
 
-	printf("\nBuscando o valor 12...\n");
+	printf("\nBuscando a chave 12...\n");
 	genericNodeBst = searchBST(rootBst, 12);
 	if (genericNodeBst)
-		printf("Valor ENCONTRADO: %i\n", genericNodeBst->key);
+		printf("Chave ENCONTRADA: %i\n", genericNodeBst->key);
 	else
-		printf("Valor NÃO encontrado\n");
+		printf("Chave NÃO encontrada\n");
 
-	printf("\n\nRemovendo o valor 5...");
+	printf("\n\nRemovendo a chave 5...");
 	rootBst = removeNodeBST(rootBst, 5);
 	printf("\npreOrder: ");
 	printBST(rootBst, "pre");
@@ -83,7 +83,7 @@ int main()
 	printf("\nposOrder: ");
 	printBST(rootBst, "pos");
 
-	printf("\n\nRemovendo o valor 8...");
+	printf("\n\nRemovendo a chave 8...");
 	rootBst = removeNodeBST(rootBst, 8);
 	printf("\npreOrder: ");
 	printBST(rootBst, "pre");
