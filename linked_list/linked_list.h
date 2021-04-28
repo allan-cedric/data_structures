0b2921f303bff6900b7cc56e7331f2d80b1b0582
Allan Cedric <allan.py3000@gmail.com>
2021-04-28 08:41:05 -0300

    change: removed success returns

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -31,43 +31,35 @@ int empty_list(linked_list_t *l);
 void destroy_list(linked_list_t *l);
 
 // === Insere uma chave 'x' no início da lista ===
-// Retorna 1 se inseriu com sucesso, senão 0.
-int push_front_list(int x, linked_list_t *l);
+void push_front_list(int x, linked_list_t *l);
 
 // === Insere uma chave 'x' no final da lista ===
-// Retorna 1 se inseriu com sucesso, senão 0.
-int push_back_list(int x, linked_list_t *l);
+void push_back_list(int x, linked_list_t *l);
 
 // === Inserção de uma chave 'x' de forma ordenada ===
-// Retorna 1 se inseriu com sucesso, senão 0.
-int push_inorder_list(int x, linked_list_t *l);
+void push_inorder_list(int x, linked_list_t *l);
 
 // === Remove a primeira chave da lista ===
-// Retorna 1 se removeu com sucesso, senão 0.
-// Além disso, retorna o elemento removido em 'item'.
-int pop_front_list(int *item, linked_list_t *l);
+// Retorna o elemento removido em 'item'.
+void pop_front_list(int *item, linked_list_t *l);
 
 // === Remove a última chave da lista ===
-// Retorna 1 se removeu com sucesso, senão 0.
-// Além disso, retorna o elemento removido em 'item'.
-int pop_back_list(int *item, linked_list_t *l);
+// Retorna o elemento removido em 'item'.
+void pop_back_list(int *item, linked_list_t *l);
 
 // === Remove uma certa chave da lista ===
-// Retorna 1 se removeu com sucesso, senão 0.
-// Além disso, retorna o elemento removido em 'item'.
-int pop_list(int key, int *item, linked_list_t *l);
+// Retorna o elemento removido em 'item'.
+void pop_list(int key, int *item, linked_list_t *l);
 
 // === Busca de uma chave na lista ===
 // Retorna 1 se encontrou, senão 0.
 int in_list(int key, linked_list_t *l);
 
 // === Concatena duas listas distintas ===
-// Retorna 1 se concatenou com sucesso, senão 0.
-int concatenate_lists(linked_list_t *l, linked_list_t *m);
+void concatenate_lists(linked_list_t *l, linked_list_t *m);
 
 // === Cria uma cópia da lista 'l' na lista 'm' ===
-// Retorna 1 se criou com sucesso, senão 0.
-int copy_list(linked_list_t *l, linked_list_t *m);
+void copy_list(linked_list_t *l, linked_list_t *m);
 
 // === Impressão de uma lista ===
 void print_list(linked_list_t *l);
