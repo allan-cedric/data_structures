98dbec0ae9d4694fa88eb71c20ef09dd5f86bf7a
Allan Cedric <allan.py3000@gmail.com>
2021-04-28 08:32:29 -0300

    feat.: new comments && changing names

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -1,111 +1,75 @@
-/* 
-   Header file : 'lista.h' 
-   Escrito por : Allan Cedric G.B. Alves da Silva
-   Profile : Aluno de graduação do curso de Ciência da Computação (UFPR)
-   GRR : 20190351
-*/
+// === Header file: linked_list.h ===
 
-#ifndef __LISTA_H__
-#define __LISTA_H__
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
 
+// === Bibliotecas ===
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-    Nodo da lista
-*/
-struct t_nodo
+// === Estrutura de dados de um nodo com chave inteira ===
+typedef struct node_t
 {
-    int chave;
-    struct t_nodo *prox;
-};
-typedef struct t_nodo t_nodo;
+    int key;
+    struct node_t *next;
+} node_t;
 
-/*
-    Estrutura p/ representar a lista
-*/
-struct t_lista
+// === Estrutura de dados: Linked list ===
+typedef struct linked_list_t
 {
-    t_nodo *ini;
-    int tamanho;
-};
-typedef struct t_lista t_lista;
+    node_t *init;
+    int size;
+} linked_list_t;
 
-/*
-    Função que inicializa uma lista.
-*/
-void cria_lista(t_lista *l);
+// === Inicializa uma lista ===
+void init_list(linked_list_t *l);
 
-/*
-    Função que retorna 1 se a lista está vazia, senão 0.
-*/
-int lista_vazia(t_lista *l);
+// === Retorna 1 se a lista está vazia, senão 0 ===
+int empty_list(linked_list_t *l);
 
-/*
-    Função que destrói uma lista.
-*/
-void destroi_lista(t_lista *l);
+// === Destrói uma lista ===
+void destroy_list(linked_list_t *l);
 
-/*
-    Função que insere um NOVO NODO com uma chave 'x' no início da lista.
-    Retorna 1 se foi bem sucedida, senão 0.
-*/
-int insere_inicio_lista(int x, t_lista *l);
+// === Insere uma chave 'x' no início da lista ===
+// Retorna 1 se inseriu com sucesso, senão 0.
+int push_front_list(int x, linked_list_t *l);
 
-/*
-    Função que insere um NOVO NODO com uma chave 'x' no fim da lista.
-    Retorna 1 se foi bem sucedida, senão 0.
-*/
-int insere_fim_lista(int x, t_lista *l);
+// === Insere uma chave 'x' no final da lista ===
+// Retorna 1 se inseriu com sucesso, senão 0.
+int push_back_list(int x, linked_list_t *l);
 
-/*
-    Função que insere um NOVO NODO com uma chave 'x' de forma ordenada.
-    Retorna 1 se foi bem sucedido, senão 0.
-*/
-int insere_ordenado_lista(int x, t_lista *l);
+// === Inserção de uma chave 'x' de forma ordenada ===
+// Retorna 1 se inseriu com sucesso, senão 0.
+int push_inorder_list(int x, linked_list_t *l);
 
-/*
-    Função que remove o primeiro nodo da lista.
-    Além disso, retorna em *item a chave removida.
-    Retorna 1 se foi bem sucedido, senão 0.
-*/
-int remove_primeiro_lista(int *item, t_lista *l);
+// === Remove a primeira chave da lista ===
+// Retorna 1 se removeu com sucesso, senão 0.
+// Além disso, retorna o elemento removido em 'item'.
+int pop_front_list(int *item, linked_list_t *l);
 
-/*
-    Função que remove o último nodo da lista.
-    Além disso, retorna em *item a chave removida.
-    Retorna 1 se foi bem sucedido, senão 0.
-*/
-int remove_ultimo_lista(int *item, t_lista *l);
+// === Remove a última chave da lista ===
+// Retorna 1 se removeu com sucesso, senão 0.
+// Além disso, retorna o elemento removido em 'item'.
+int pop_back_list(int *item, linked_list_t *l);
 
-/*
-    Função que remove um nodo com uma certa 'chave' da lista.
-    Além disso, retorna em *item a chave removida.
-    Retorna 1 se foi bem sucedido, senão 0.
-*/
-int remove_item_lista(int chave, int *item, t_lista *l);
+// === Remove uma certa chave da lista ===
+// Retorna 1 se removeu com sucesso, senão 0.
+// Além disso, retorna o elemento removido em 'item'.
+int pop_list(int key, int *item, linked_list_t *l);
 
-/*
-    Função que busca um nodo com uma certa 'chave' na lista.
-    Retorna 1 se encontrou, senão 0.
-*/
-int pertence_lista(int chave, t_lista *l);
+// === Busca de uma chave na lista ===
+// Retorna 1 se encontrou, senão 0.
+int in_list(int key, linked_list_t *l);
 
-/*
-    Função que concatena uma lista 'm' na lista 'l'.
-    Retorna 1 se foi bem sucedida, senão 0.
-*/
-int concatena_listas(t_lista *l, t_lista *m);
+// === Concatena duas listas distintas ===
+// Retorna 1 se concatenou com sucesso, senão 0.
+int concatenate_lists(linked_list_t *l, linked_list_t *m);
 
-/*
-    Função que cria uma cópia (lista 'm') de uma
-    lista 'l'. Retorna 1 se foi bem sucedida, senão 0.
-*/
-int copia_lista(t_lista *l, t_lista *m);
+// === Cria uma cópia da lista 'l' na lista 'm' ===
+// Retorna 1 se criou com sucesso, senão 0.
+int copy_list(linked_list_t *l, linked_list_t *m);
 
-/*
-    Função que imprime uma lista.
-*/
-void imprime_lista(t_lista *l);
+// === Impressão de uma lista ===
+void print_list(linked_list_t *l);
 
 #endif
