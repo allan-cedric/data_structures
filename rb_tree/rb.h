a773497a8fdf1953b1382db1090601e4561c0e6b
Allan Cedric <allan.py3000@gmail.com>
2021-05-01 17:24:50 -0300

    starting the header file (rb_tree)

---
+++ rb_tree/rb.h
@@ -0,0 +1,59 @@
+// === Header file: rb.h ===
+
+#ifndef __RB_H__
+#define __RB_H__
+
+// === Bibliotecas ===
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// === Conjunto enumerado do sistema de cores da Red-Black Tree ===
+typedef enum rbtree_node_color
+{
+    BLACK,
+    RED
+} rbtree_node_color;
+
+// === Estrutura de dados de um nodo da Red-Black Tree ===
+typedef struct rbtree_node_t
+{
+    int key;
+    rbtree_node_color color;
+    struct rbtree_node_t *left, *right, *parent;
+} RBTree;
+
+// === Inicializa uma Red-Black Tree ===
+RBTree *createRBTree();
+
+// === Retorna 1 se a Red-Black Tree está vazia, senão 0 ===
+int emptyRBTree(RBTree *root);
+
+// === Cria um novo nodo para uma Red-Black Tree ===
+// Retorna um ponteiro para nodo.
+RBTree *newNode(int key);
+
+// === Destrói uma Red-Black Tree ===
+RBTree *destroyRBTree(RBTree *root);
+
+// === Imprimi uma Red-Black Tree ===
+// Possui 3 opções (const char op): "pre", "in" e "pos".
+void printRBTree(RBTree *root, const char *op);
+
+// === Busca um certo elemento em uma Red-Black Tree ===
+// Retorna um ponteiro para o nodo encontrado, senão NULL.
+RBTree *searchRBTree(RBTree *root, int key);
+
+// === Retorna um ponteiro para nodo com o menor elemento ===
+RBTree *minRBTree(RBTree *root);
+
+// === Retorna um ponteiro para nodo com o maior elemento ===
+RBTree *maxRBTree(RBTree *root);
+
+// === Rotação para direita (Caso Esquerda-Esquerda) ===
+RBTree *LLRotationRBTree(RBTree *root, RBTree *node_x);
+
+// === Rotação para direita (Caso Direita-Direita) ===
+RBTree *RRRotationRBTree(RBTree *root, RBTree *node_x);
+
+#endif
